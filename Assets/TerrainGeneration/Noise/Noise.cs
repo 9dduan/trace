f905ebeabec6ee6a59d0288627cbb6a686dd79e5
9dduan <duanduan173@gmail.com>
2022-02-09 01:01:59 +0800

    13

--- Assets/TerrainGeneration/Noise/Noise.cs
+++ Assets/TerrainGeneration/Noise/Noise.cs
@@ -7,9 +7,19 @@ using System.Collections;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight,int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
 	{
         float[,] noiseMap = new float[mapWidth, mapHeight];
+		Vector2[] octaveOffsets = new Vector2[octaves];
+
+		System.Random prng = new System.Random(seed);
+
+		for (int i=0; i < octaves; i++)
+        {
+			float offsetX = prng.Next(-100000, 100000) + offset.x;
+			float offsetY = prng.Next(-100000, 100000) + offset.y;
+			octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
 		if (scale <= 0)
 		{
@@ -18,6 +28,9 @@ public static class Noise
 
 		float maxNoiseHeight = float.MinValue;
 		float minNoiseHeight = float.MaxValue;
+
+		float halfWidth = mapWidth / 2f;
+		float halfHeight = mapHeight / 2f;
 
         for(int y = 0; y < mapHeight; y++)
 		{
@@ -29,19 +42,20 @@ public static class Noise
 
                 for(int i = 0; i < octaves; i++)
                 {
-					float sampleX = x / scale * frequency;
-					float sampleY = y / scale * frequency;
+					float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+					float sampleY = (y - halfHeight)/ scale * frequency + octaveOffsets[i].y;
 
 					/**
-                        use octave to mix perlin values
+                        simplex noise 
+                        mix octaves of perlin noise 
 					    https://www.cnblogs.com/babyrender/archive/2008/10/27/BabyRender.html
                     */  
 
-					float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+					float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1; // perlinNoise return value from 0 to 1, we want to have -1 to 1
 					noiseHeight += perlinValue * amplitude;
 
 					amplitude *= persistance;
-					frequency *= lacunarity;
+					frequency *= lacunarity; // higher frequency => more separated sampled positions => fuzzier wave
 				}
 
                 if(noiseHeight > maxNoiseHeight)
